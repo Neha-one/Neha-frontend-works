65760412f32b8380d6225532172245ee3a4fc9e2
Neha-one <justforgtb@gmail.com>
2025-11-11 18:31:43 +0530

    add html , css and js file

---
+++ 47 Introduction.JS/new.js
@@ -0,0 +1,9 @@
+console.log("Hello world")
+console.log("code is running....")
+console.log("code is also running....")
+console.log("code is looking like a wow....")
+
+document.title = "Neha"
+
+// add inline css
+document.body.style.backgroundColor = "red"
---
+++ 47 Introduction.JS/script.js
@@ -0,0 +1,19 @@
+alert("welcome to my wisdom-")
+
+console.log("code is running....")
+console.log("code is also running....")
+console.log("code is looking like a wow....")
+
+
+var a = prompt("Enter your number")
+
+var istrue = confirm("Are you sure you want to leave this page and blast your computer.")
+if (istrue) {
+    console.log("Computer is blasting")
+}
+else {
+
+    console.log("Computer is not blasting")
+}
+console.log("Your number is " + a);
+
