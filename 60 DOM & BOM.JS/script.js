a8781f1407ba6fbf11d2eb5bb9a49d4bc0dde05c
Neha-one <justforgtb@gmail.com>
2025-11-16 02:39:38 +0530

    add html , css and js file

---
+++ 60 DOM & BOM.JS/script.js
@@ -0,0 +1,23 @@
+//we can change the intire html code using document function 
+// change the title of this file
+document.title = "Neha is good"
+
+//print on console the title-
+console.log(document.title)     //In console :-Neha is good \
+
+//print on console the intire body of this html file-
+console.log(document.body)  //In console :-<body></body> (and we can see all code inside)
+
+// Change CSS directly using document()-
+document.body.style.backgroundColor = "black";      //it add inline CSS
+document.body.style.color = "white";
+
+
+// WINDOW is a global object. In console:- (window.document.body)  then it give us body
+
+
+// $0
+// we can select any thing and after that it will make $0 and after that we can use $0 as a document and apply properties
+// %0.style.backgroundcolor="orange"        and it will make that selected area in orange.
+
+
