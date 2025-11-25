dfc16857a8cbd79c44a022d50a4cb5368f8dab17
Neha-one <justforgtb@gmail.com>
2025-11-26 01:52:06 +0530

    update html , css and js file

--- 71 Fetch-API & Requests.JS/Fetch-Api.js
+++ 71 Fetch-API & Requests.JS/Fetch-Api.js
@@ -11,11 +11,81 @@ const URL = "https://fakestoreapi.com/carts";
 // getfacts();
 
 //OR---------
-async function getfact() {
-    console.log('getting data....');
 
-    let promise =await  fetch(URL);
-    console.log(promise);
-    // console.log(promise.status);     //200
+// async function getfact() {
+//     console.log('getting data....');
+
+//     let promise = await fetch(URL);
+//     console.log(promise);       //JSON format
+//     // console.log(promise.status);     //200
+// }
+// getfact();
+
+const factPara = document.querySelector("#fact");
+const Btn = document.querySelector("#btn");
+
+//using async-await-----------SIMPLE AND EASY TO UNDERSTAND:-
+// async function getData() {
+//     console.log('getting data........');
+//     let respose = await fetch(URL);
+//     console.log(respose);
+//     let data = await respose.json();
+//     // console.log(data);
+//     // console.log(data[1]);
+//     // console.log(data[0].products);
+
+//     factPara.innerText = data[0].date
+// };
+
+
+//using promise---------------HARD AND COMPLEX
+function getData() {
+    fetch(URL).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data[0]);
+        factPara.innerText = data[0].date
+    });
 }
-getfact();
+Btn.addEventListener("click", getData)
+
+
+// settle means resolve or reject
+// resolve means promise has settld successfully
+// reject means promise has not settled successfully
+async function getData() {
+    let respose = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    //.json() return data in form of js object.
+    let data = await respose.json();
+    //.text()  return data in form of text.
+    // let data=await respose.text();
+    console.log(data);
+    //we can also print data direct from return
+    // return data;    //print from main() console.log(data);
+    return 56;
+}
+async function main() {
+    console.log('data loaded');
+    let get = await getData()
+    //this console-get is for return of getData() otherwise it show undefined without any return;
+    console.log(get);
+    console.log('data processing');
+}
+main();
+
+// WE CAN TAKE URL AS A INPUT :-
+async function getData2(url) {
+    let respose = await fetch(url);
+    let data = await respose.json();
+    return data;
+
+}
+async function main2() {
+    console.log('data loaded');
+    let url = "https://jsonplaceholder.typicode.com/todos/1";
+    let get = await getData2(url)
+    console.log(get);
+    console.log('data processing');
+}
+main2();
+
--- 71 Fetch-API & Requests.JS/Requests.js
+++ 71 Fetch-API & Requests.JS/Requests.js
@@ -1,16 +1,16 @@
-//TYPES OF REQUESTS:-
+// TYPES OF REQUESTS:-
 
-//          🟢 1. GET Request
+//   1. GET Request----Give me the data.
 
-//by default.
-//just ask for for data.(ask for menu.)
+// by default.
+// just ask for for data.(ask for menu.)
 
-// fetch("https://jsonplaceholder.typicode.com/todos/1")
-// .then(response=>response.json())
-// .then(data=>console.log(data));
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log("error", err));
 
-
-//          🟢 2. POST Request
+//           2. POST Request----I want to add something new.
 
 //You send data to the server (like creating a new item).
 
@@ -19,7 +19,49 @@ let getData = fetch("https://jsonplaceholder.typicode.com/posts", {
     headers: { "content-Type": "application/json" },
     body: JSON.stringify({ title: "Hello", body: "world", userId: 1 })
 })
-getData.then(response => response.json())
-    .then(data => console.log(data));
+getData
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log("error", err)
+    );
 
-    //output    {title: 'Hello', body: 'world', userId: 1, id: 101}
+//output    {title: 'Hello', body: 'world', userId: 1, id: 101}
+
+let getdata = fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify({ title: "hello", body: "world" })
+})
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log("error hai bhai", err)
+    );
+
+
+//           3. PUT Request----I want Update data/modify existing data on the server.
+
+let getdata3 = fetch("https://jsonplaceholder.typicode.com/posts/1",
+    {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "put": "neha",
+            age: 21
+        })
+    }
+)
+    .then((res) => res.json()).then((data) => console.log(data))
+    .catch((err) => console.log("error", err)
+    );
+
+
+//           4. DELETE Request----I want data.
+let getdata4 = fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE"
+})
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log('error', err)
+    );
