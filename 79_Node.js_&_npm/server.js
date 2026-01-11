fb97c5f670437e042d971d817a9d47d7698bc5af
Neha-one <justforgtb@gmail.com>
2026-01-12 00:19:34 +0530

    delete these files

--- 79_Node.js_&_npm/server.js
+++
@@ -1,8 +0,0 @@
-var slugify = require('slugify')
-
-let a=slugify('Neha Kumari') // some-string
-console.log(a);
-
-// if you prefer something other than '-' as separator
-let b=slugify('Neha &^Kumari', '_')  // some_string
-console.log(b);
